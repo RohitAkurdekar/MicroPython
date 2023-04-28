ca1a09629f6a6537f57948b8c53ff8b96aa243ef
RohitAkurdekar <rakurdekar@gmail.com>
2023-04-28 22:20:09 +0530

    I2C GLCD

---
+++ ESP32/GLCD/README.md
@@ -0,0 +1,16 @@
+# I2C Graphic LCD
+
+* Copy all the required files to ESP32 or any MicroPython supporting board.
+    * ssd1306.py
+    * writer.py
+    * freesans20.py
+    * main.py
+
+------------------------------------------------------------------------
+
+<img src = "./GLCD_display.jpg">
+
+
+-------------------------------------------------------------------------
+
+Rohit Akurdekar
--- ESP32/GLCD/main.py
+++ ESP32/GLCD/main.py
@@ -1,9 +1,39 @@
+'''
+Name        : i2c_GLCD.py
+Date        : 25/Apr/2023
+Brief       : Interface GLCD with ESP32 and display text.
+Tested      : OK
+Developer   : Rohit Akurdekar
+'''
+
 from machine import Pin, I2C
-import ssd1306
+from ssd1306 import SSD1306_I2C
+import freesans20
+import writer
 
-# using default address 0x3C
+# Display dimensions
+WIDTH  = 128                                          
+HEIGHT = 64                                             
+
+# Display I2C pins
 i2c = I2C(sda=Pin(21), scl=Pin(22))
-display = ssd1306.SSD1306_I2C(128, 64, i2c)
+display = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
-display.text('Hello, World!', 0, 0, 1)
+# Draw 
+display.fill(0)
+display.fill_rect(0, 0, 32, 32, 1)
+display.fill_rect(2, 2, 28, 28, 0)
+display.vline(9, 8, 22, 1)
+display.vline(16, 2, 22, 1)
+display.vline(23, 8, 22, 1)
+display.fill_rect(26, 24, 2, 4, 1)
+# display.text('MicroPython', 40, 0, 1)
+display.text('ESP32 with', 40, 12, 1)
+# display.text('OLED 128x64', 40, 24, 1)
+
+# Display text with larger font
+font_writer = writer.Writer(display, freesans20)
+font_writer.set_textpos(5, 40)
+font_writer.printstring("MicroPython")
+
 display.show()
